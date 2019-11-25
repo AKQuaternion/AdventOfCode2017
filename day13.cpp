a3bd3b7e13ced20bb07110749bad56fca2957c2f
Chris Hartman <chris.m.hartman@gmail.com>
2019-11-24 15:06:11 -0900

    Do day 13

--- day13.cpp
+++ day13.cpp
@@ -35,12 +35,15 @@ using std::string;
 using std::swap;
 using std::vector;
 
-
 void day13() {
-  map<int, int> m;
+  map<int, int> scanners;
   auto star1 = 0;
   auto star2 = 0;
   ifstream ifile("../day13.txt");
+  //  istringstream ifile("0: 3\n"
+  //                      "1: 2\n"
+  //                      "4: 4\n"
+  //                      "6: 4");
   string line;
   while (getline(ifile, line)) {
     string _s;
@@ -48,10 +51,23 @@ void day13() {
     istringstream iline(line);
     int layer, range;
     iline >> layer >> _c >> range;
-    cout << layer << " " << range << endl;
+    scanners[layer] = range;
+    //    cout << layer << " " << range << endl;
   }
-  //    cout << endl;
-}
-cout << "Day 13 star 1 = " << star1 << "\n";
-cout << "Day 13 star 2 = " << star2 << "\n";
+
+  auto initialScanners = scanners;
+
+  for (const auto [l, r] : scanners)
+    if (l % (2 * (r - 1)) == 0)
+      star1 += l * r;
+
+  bool caught = (star1 != 0);
+  while (caught) {
+    ++star2;
+    caught = false;
+    for (const auto [l, r] : scanners)
+      caught |= ((l + star2) % (2 * (r - 1)) == 0);
+  }
+  cout << "Day 13 star 1 = " << star1 << "\n";
+  cout << "Day 13 star 2 = " << star2 << "\n";
 }
